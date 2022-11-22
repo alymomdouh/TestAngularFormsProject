70b142d8a8d022f2f94386c5fdc2442872941dc5
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2022-11-22 22:58:12 +0200

    add data model and two way binding

--- src/app/user-setting-form/user-setting-form.component.ts
+++ src/app/user-setting-form/user-setting-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-setting';
 
 @Component({
   selector: 'app-user-setting-form',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-setting-form.component.css']
 })
 export class UserSettingFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
 
+  userSettings: UserSettings = { ...this.originalUserSettings };
   constructor() { }
 
   ngOnInit(): void {
